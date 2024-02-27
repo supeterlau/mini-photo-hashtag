2d78e431064f732af42b0f9494c5cea8d66a9c45
Peter Lau <superpeterlau@outlook.com>
2024-02-27 23:01:58 +0800

    deploy: fix valid type

--- src/app/photo/layout.tsx
+++ src/app/photo/layout.tsx
@@ -1,10 +1,11 @@
 import PhotoLayout from './components/PhotoLayout';
 import './layout.css'
 
-import { Metadata, ResolvingMetadata } from 'next';
+import { Metadata } from 'next';
+// import { Metadata, ResolvingMetadata } from 'next';
 
 export async function generateMetadata(
-  parent: ResolvingMetadata
+  // parent: ResolvingMetadata
 ): Promise<Metadata> {
   return  {
     title: "Photos",
