5291b3a444d9be08669766b3f4a8708efa138c2d
Peter Lau <superpeterlau@outlook.com>
2024-02-28 07:07:57 +0800

    fix project url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
         <a
-          href="https://mini-photo-hashtag.vercel.app/"
+          href="https://mini-photo-hashtag.vercel.app/photo"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
