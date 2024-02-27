7e631a4ef6e3a99f391f7a4ad7ca42a05bb70371
Peter Lau <superpeterlau@outlook.com>
2024-02-27 22:44:33 +0800

    fix PhotoDetail.tsx

--- src/app/photo/[slug]/page.tsx
+++ src/app/photo/[slug]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import PhotoDetails from '../components/PhotoDetails'
+import PhotoDetails from '../components/PhotoDetail'
 
 export default function Page({ params }: { params: { slug: string } }) {
   return (
